33775d6404d4b73b5566da719cabb65aa4965a6f
chuckchan24 <617229456@qq.com>
2018-05-13 12:27:13 +0800

    step one-3: deal with db: create a para without input app and use db.init_app(app) in function create_app

--- ihome/__init__.py
+++ ihome/__init__.py
@@ -8,6 +8,9 @@ from flask_wtf import CSRFProtect
 from cofig import config_dict
 
 
+db = SQLAlchemy()
+
+
 # 工厂方法：传入不同参数，返回不同的对象
 def create_app(config_name):
     """根据不同开发阶段，创建不同类型config配置的app对象"""
@@ -19,7 +22,7 @@ def create_app(config_name):
     app.config.from_object(config_type)
 
     # 创建sqlalchemy对象
-    db = SQLAlchemy(app)
+    db.init_app(app)
 
     # 创建redis数据库链接对象
     redis_store = redis.StrictRedis(host=config_type.REDIS_HOST,
--- manager.py
+++ manager.py
@@ -1,8 +1,7 @@
 # coding=utf-8
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
-from ihome import db
-from ihome import create_app
+from ihome import create_app, db
 
 # 需求：不修改业务逻辑代码，只通过修改manager.py的代码
 app = create_app('development')
