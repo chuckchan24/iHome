dea8f3c8c6c3cf71a85611888a4462e7ce025011
chuckchan24 <617229456@qq.com>
2018-05-21 21:35:59 +0800

    step two-11: additional expansion of singleton mode of class CPP in sms.py

--- ihome/utils/sms.py
+++ ihome/utils/sms.py
@@ -26,12 +26,27 @@ softVersion = '2013-12-26'
 
 
 class CCP(object):
+    # cls._instance
+    def __new__(cls, *args, **kwargs):
+        # 判断cls是否拥有_instance属性，此属性用来保存这个类的唯一对象（单例模式）
+        if not hasattr(cls, '_instance'):
+            # 创建一个单例对象
+            obj = super(CCP, cls).__new__(cls)
+            # 初始化REST SDK
+            obj.rest = REST(serverIP, serverPort, softVersion)
+            obj.rest.setAccount(accountSid, accountToken)
+            obj.rest.setAppId(appId)
 
-    def __init__(self):
-        # 初始化REST SDK
-        self.rest = REST(serverIP, serverPort, softVersion)
-        self.rest.setAccount(accountSid, accountToken)
-        self.rest.setAppId(appId)
+            cls._instance = obj
+
+        # 如果有_instance属性，直接返回
+        return cls._instance
+
+    # def __init__(self):
+    #     # 初始化REST SDK
+    #     self.rest = REST(serverIP, serverPort, softVersion)
+    #     self.rest.setAccount(accountSid, accountToken)
+    #     self.rest.setAppId(appId)
 
     # 发送模板短信
     # @param to 手机号码
